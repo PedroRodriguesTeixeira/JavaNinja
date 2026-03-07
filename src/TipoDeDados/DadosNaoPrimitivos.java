240b2a2c20ab430386ec7914f6a1157f762c8e36
Pedro Teixeira <pedro51teixeira@gmail.com>
2026-03-07 19:20:42 -0300

    Repositório para aprender java

---
+++ src/TipoDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,9 @@
+package TipoDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados não primitivos: String, Array, Class, Enum
+        * */
+    }
+}
---
+++ src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,28 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+
+    public static void main(String[] args) {
+
+        /*
+        *
+        * Dados primitivos - int, double, float, char, boolean, short.
+        * Objetivo da aula: Criar um ninka - Naruto
+        *
+        * */
+
+        int idade = 16; // Valor máximo: 2 147 438 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuNao = true;
+        Long saldoBancario = 999999L; // O valor máximo do 9.223.372.036.854.775.807
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(inicial);
+        System.out.println(vivoOuNao);
+        System.out.println("saldo Bancario é = " + saldoBancario);
+
+    }
+
+}
