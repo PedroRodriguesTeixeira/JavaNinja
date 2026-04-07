6c89b45925ca021406645ec5e0d2d94b35a9651f
Pedro Teixeira <pedro51teixeira@gmail.com>
2026-04-07 20:46:18 -0300

    Aulas de java do módulo inicial

---
+++ src/NivelBasico/TipoDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,18 @@
+package TipoDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados não primitivos: String, Array, Class, Enum
+        * */
+
+        String nome = "Naruto Uzumaki";
+        String nomeToUpperCase = nome.toUpperCase(); //ToUppercase vai colocar tudo em CAPSLOCK
+        System.out.println("Esse textoe esta em CAPSLOCK: " + nomeToUpperCase);
+        System.out.println("Esse texto esta normal: " + nome);
+
+        String aldeia = "ALDEIA DA FOLHA";
+        String aldeiaEmCaixaBaixa = aldeia.toLowerCase(); //ToLowerCase vai colocar tudo em caixa baixa/minusculo.
+        System.out.println(aldeia);
+    }
+}
--- src/TipoDeDados/DadosNaoPrimitivos.java
+++
@@ -1,9 +0,0 @@
-package TipoDeDados;
-
-public class DadosNaoPrimitivos {
-    public static void main(String[] args) {
-        /*
-        * Dados não primitivos: String, Array, Class, Enum
-        * */
-    }
-}
