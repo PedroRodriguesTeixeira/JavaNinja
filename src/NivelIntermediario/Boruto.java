6bd45ef83325a1b283356c5c30134976e96c4944
Pedro Teixeira <pedro51teixeira@gmail.com>
2026-05-14 09:30:59 -0300

    Alteracoes na classe boruto

--- src/NivelIntermediario/Boruto.java
+++ src/NivelIntermediario/Boruto.java
@@ -2,12 +2,12 @@ package NivelIntermediario;
 
 public class Boruto extends Uzumaki implements HyuugaUzumaki {
 
-    @Override
+
     public void AtivarOKarma() {
         System.out.println("O karma foi ativado! Eu sou um Hyuuga Uzumaki.");
     }
 
-    @Override
+
     public void AtivarJougan() {
         System.out.println("O Jougan foi ativado! Eu sou um Hyuuga Uzumaki.");
     }
