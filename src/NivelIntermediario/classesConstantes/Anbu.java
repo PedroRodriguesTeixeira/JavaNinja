be6cc2157726dccd1e545c98780092bf642918d3
Pedro Teixeira <pedro51teixeira@gmail.com>
2026-07-19 11:58:40 -0300

    Aula sobre modificador final em classes

---
+++ src/NivelIntermediario/classesConstantes/Anbu.java
@@ -0,0 +1,10 @@
+package NivelIntermediario.classesConstantes;
+
+final public class Anbu {
+
+    String nome;
+
+    public void anbu(){
+        System.out.println("Eu sou um anbu.");
+    }
+}
---
+++ src/NivelIntermediario/classesConstantes/Main.java
@@ -0,0 +1,11 @@
+package NivelIntermediario.classesConstantes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Anbu anbuNninja = new Anbu();
+        anbuNninja.nome = "Kakashi Hatake";
+        System.out.println(anbuNninja.nome);
+    }
+
+}
