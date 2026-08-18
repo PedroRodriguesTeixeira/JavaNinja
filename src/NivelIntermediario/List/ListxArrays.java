98be7e666ec59abe8e8c74b22c1fb0f9cf450556
Pedro Teixeira <pedro51teixeira@gmail.com>
2026-08-18 08:15:22 -0300

    Estudando e entendendo sobre listas

---
+++ src/NivelIntermediario/List/ListxArrays.java
@@ -0,0 +1,23 @@
+package NivelIntermediario.List;
+
+import java.util.List;
+
+public class ListxArrays {
+
+    public static void main(String[] args) {
+        //Array
+        //São estáticos, nçao alteram de tamanhno
+        String[] ninjaArray = new String[4];
+        ninjaArray[0] = "Naruto";
+        ninjaArray[1] = "Sasuke";
+        ninjaArray[2] = "Sakura";
+
+        System.out.println("Pritando ninja 1: " + ninjaArray[0]);
+        System.out.println("Pritando posição vazia: " + ninjaArray[3]);
+
+        //Listas
+        //Listas não são estáticas, elas podem aumentar ou diminuir
+
+    }
+
+}
